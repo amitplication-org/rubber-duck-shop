5c44d166caf82869c9714946c468e0ffb39f5485
Amit Barletz <amit@amplication.com>
2022-07-28 22:55:57 +0300

    feat(client) create react app with ts and scss & clean up

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        rubber duck shop
+      </header>
+    </div>
+  );
+}
+
+export default App;
